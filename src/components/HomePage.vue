e951097a675bf14653a6692f724f7026f6d3a89f
周诚 <zhoucheng133@outlook.com>
2022-10-07 09:20:02 +0800

    first submit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        {{msg}}
+    </div>
+</template>
+
+<script>
+export default {
+    name:"homepage",
+    data(){
+        return{
+            
+        }
+    },
+    methods: {
+        
+    },
+}
+</script>
+<style scoped>
+
+</style>
